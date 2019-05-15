5768796132baabb776d7afe0b5f896e9dfd6c452
“Pina <pina.gornik@student.um.si>
2019-05-15 15:26:59 +0200

    dodan front-end

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,19 @@ import Container from "reactstrap/es/Container";
 import Navigation from "./components/layout/Navigation";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Album from "./components/layout/Album";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+
             <Navigation/>
+            <Album/>
+
             <Container>
 
-                <h1>Welcome to Praktikum II template.</h1>
-                <p>Congratulations on making it this far, Marko is proud of you.</p>
             </Container>
-        <Footer/>
+
         </div>
     );
 }
