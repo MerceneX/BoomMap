961c5db144c909b895f2395933ea2d6cd7b16c5a
Marko <sef.gluhak@gmail.com>
2019-06-03 13:12:23 +0100

    Popravilo CORS #2

--- podatki/unique_values.py
+++ podatki/unique_values.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv('MPDOGOD2014.csv')
-unique_values = data['LONO'].unique()
+data = pd.read_csv("MPDOGOD2014.CSV", delimiter=";")
+unique_values = data['PRPO'].unique()
 
 print(unique_values)
