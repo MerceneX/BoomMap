77a24ee92a1de0a44936a7ce537706ccaa49fa9d
Marko <sef.gluhak@gmail.com>
2019-06-04 00:09:02 +0100

    Dodani endpointi na id 24,25,26

--- podatki/unique_values.py
+++ podatki/unique_values.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 data = pd.read_csv("MPDOGOD2014.CSV", delimiter=";")
-unique_values = data['PRPO'].unique()
+unique_values = data['PRVR_Vreme'].unique()
 
 print(unique_values)
--- routes/graphRoutes.js
+++ routes/graphRoutes.js
@@ -6,27 +6,158 @@ const express = require("express"),
 
 router.get("/23", (req, res) => {
     res.json({
-        title: "Nesreče skozi leta",
-        data: [
+        naslov: "Nesreče skozi leta",
+        podatki: [
             {
-                year: "2011",
-                accidents: "188"
+                leto: "2014",
+                nesrece: "24345"
             },
             {
-                year: "2012",
-                accidents: "200"
+                leto: "2015",
+                nesrece: "25654"
             },
             {
-                year: "2013",
-                accidents: "132"
+                leto: "2016",
+                nesrece: "23569"
             },
             {
-                year: "2014",
-                accidents: "100"
+                leto: "2017",
+                nesrece: "25234"
+            },
+            {
+                leto: "2018",
+                nesrece: "26615"
             }
         ]
     })
 });
 
+router.get("/24", (req, res) => {
+    res.json({
+        title: "Nesreče po dnevih leta",
+        data: [
+            {
+                dan: "pon",
+                nesrece: "10271"
+            },
+            {
+                dan: "tor",
+                nesrece: "8251"
+            },
+            {
+                dan: "sre",
+                nesrece: "7514"
+            },
+            {
+                dan: "čet",
+                nesrece: "8132"
+            },
+            {
+                dan: "pet",
+                nesrece: "55172"
+            },
+            {
+                dan: "sob",
+                nesrece: "32183"
+            },
+            {
+                dan: "ned",
+                nesrece: "13714"
+            }
+        ]
+    })
+});
+
+router.get("/25", (req, res) => {
+    res.json({
+        title: "Nesreče glede na vreme",
+        data: [
+            {
+                vreme: "deževno",
+                nesrece: "21398"
+            },
+            {
+                vreme: "jasno",
+                nesrece: "45154"
+            },
+            {
+                vreme: "megla",
+                nesrece: "8917"
+            },
+            {
+                vreme: "neznano",
+                nesrece: "10361"
+            },
+            {
+                vreme: "oblačno",
+                nesrece: "4913"
+            },
+            {
+                vreme: "sneg",
+                nesrece: "8509"
+            },
+            {
+                vreme: "toča",
+                nesrece: "1498"
+            },
+            {
+                vreme: "veter",
+                nesrece: "7209"
+            }
+        ]
+    })
+});
+
+router.get("/26", (req, res) => {
+    res.json({
+        title: "Nesreče glede na vzrok",
+        data: [
+            {
+                vzrok: "nepravilnost na cesti",
+                nesrece: "21398"
+            },
+            {
+                vzrok: "neprilagojena hitrost",
+                nesrece: "45154"
+            },
+            {
+                vzrok: "nepravilnost pešca",
+                nesrece: "1465"
+            },
+            {
+                vzrok: "ostalo",
+                nesrece: "10361"
+            },
+            {
+                vzrok: "neupoštevanje pravil o prednosti",
+                nesrece: "4213"
+            },
+            {
+                vzrok: "nepravilno prehitevanje",
+                nesrece: "3617"
+            },
+            {
+                vzrok: "premiki z vozilom",
+                nesrece: "1498"
+            },
+            {
+                vzrok: "nepravilna stran / smer vožnje",
+                nesrece: "1513"
+            },
+            {
+                vzrok: "nepravilnost na tovoru",
+                nesrece: "186"
+            },
+            {
+                vzrok: "nepravilnost na vozilu",
+                nesrece: "209"
+            },
+            {
+                vzrok: "neustrezna varnostna razdalja",
+                nesrece: "7209"
+            }
+        ]
+    })
+});
 
 module.exports = router;
