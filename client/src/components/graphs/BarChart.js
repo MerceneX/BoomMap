89a422332ae47e42714380d95e2c3613cee45668
“Pina <pina.gornik@student.um.si>
2019-06-03 14:34:31 +0200

    graf

--- client/src/components/graphs/BarChart.js
+++ client/src/components/graphs/BarChart.js
@@ -7,26 +7,8 @@ import {
 
 var data = [
     {
-        name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-        name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-        name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-        name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-        name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-        name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-        name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
+
+    }
 ];
 
 
@@ -39,11 +21,14 @@ export default class Test extends PureComponent {
     componentDidMount() {
         axios.get('http://localhost:5000/api/graph/23').then(res => {
             this.setState({datag: res.data});
-            console.log(res.data);
-            for(var key in data) {
-                data.push(data[key]);
-                console.log(data[key]);
+            //console.log(res.data);
+            for(var key in this.state) {
+                data.push(this.state[key]);
+                console.log(this.state[key]);
+                //this.setState({datag: data[key]});
+
             }
+
         });
     }
 
@@ -52,18 +37,18 @@ export default class Test extends PureComponent {
             <BarChart
                 width={500}
                 height={300}
-                data={data}
+                data={this.state.datag.data}
                 margin={{
                     top: 5, right: 30, left: 20, bottom: 5,
                 }}
             >
                 <CartesianGrid strokeDasharray="3 3"/>
-                <XAxis dataKey="name"/>
+                <XAxis dataKey="year"/>
                 <YAxis/>
                 <Tooltip/>
                 <Legend/>
-                <Bar dataKey="pv" fill="#8884d8"/>
-                <Bar dataKey="uv" fill="#82ca9d"/>
+                <Bar dataKey="accidents" fill="#8884d8"/>
+
             </BarChart>
         );
     }
