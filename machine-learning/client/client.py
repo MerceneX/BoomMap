7d7354fd3e1ab563da5e13ae42f3af57abbac25e
marusakonecnik <konecnik.10@gmail.com>
2019-06-05 12:20:41 +0200

    odstranjeni presledki, pripravljen output za algoritem

--- machine-learning/client/client.py
+++ machine-learning/client/client.py
@@ -1,20 +1,13 @@
-import requests, data_processer as dp
-import json
+import requests, data_processer as dp, json,csv
 import pandas as pd
-import json
-import csv
-import datetime
-import codecs
-from pandas.io.json import json_normalize
 from efficient_apriori import apriori
 import numpy as nm
-import holidays as h
 
 
 # testing purposes
 def test(data):
     data_to_observe = data[['stevilka_odseka', 'regija']]
-    #data_to_observe = data_to_observe.loc[data['PRVR_Vreme']!='J']
+    # data_to_observe = data_to_observe.loc[data['PRVR_Vreme']!='J']
     data_to_observe.to_csv("clean_data.csv", encoding='utf-8', index=False)
     itemsets, rules = apriori(dp.data_generator('clean_data.csv'), min_support=0.2, min_confidence=0.3)
     print('o')
@@ -22,15 +15,27 @@ def test(data):
 
 
 # data to send
-def post_data(data):
-    sections, sections_numbers, town = dp.get_road_data(data)
-    itemsets, rules = create_rules()
+def post_data():
+    with open('output.json') as json_file:
+        data = json.load(json_file)
+
     url = ''
-    jsonData = {
+
+    # requests.post(url,)
+    return 0
+
+
+def save_output(data, itemsets):
+    common_itemsets = list()
+    for i in range(len(itemsets)):
+        common_itemsets.append(list(itemsets[i + 1].keys()))
+    sections, sections_numbers, town, region = dp.get_road_data(data)
+    json_data = {
         'critical_sections': sections,
         'critical_sections_number': sections_numbers,
-        'common-sets': itemsets,
+        'itemsets': common_itemsets,
         'town': town,
+        'region': region,
         'legend': {
             '0': 'poskodbe',
             '1': 'datum',
@@ -55,13 +60,12 @@ def post_data(data):
 
         }
     }
-    print(jsonData)
-    # requests.post(url,)
-    return 0
+    with open('output.json', 'w') as outfile:
+        json.dump(json_data, outfile)
 
 
 # use of apriori algorithm
-def create_rules():
+def create_rules(data,path):
     joined = read_join_data()
     joined.to_csv("clean_data.csv", encoding='utf-8', index=False)
     itemsets, rules = apriori(dp.data_generator('clean_data.csv'), min_support=0.2, min_confidence=0.5)
@@ -70,30 +74,52 @@ def create_rules():
 
 
 # reads data and joins,filters it and joins it to a single dataframe
-def read_join_data():
+def read_join_data(filename):
     dataframes = []
     year = 14
-    codes = pd.read_csv('../../podatki/MPSIFR.CSV', delimiter=";")
-    car_type_codes = codes.loc[codes['besedna_sifra'] == 'LVZN']
 
     for i in range(5):
-        path = '../../podatki/MPDOGOD20' + str(year) + '.CSV'
+        path = '../../podatki/'+filename + str(year) + '.CSV'
         dataframes.append(pd.read_csv(path, delimiter=";"))
         year += 1
     joined = pd.concat(dataframes, ignore_index=True, sort=False)
+    '''
     months, days, weekdays, holidays = dp.split_date(joined['Datum_Nesrece'])
     joined['mesec'] = pd.Series(nm.array(months))
     joined['dan'] = pd.Series(nm.array(days))
     joined['dan_v_tednu'] = pd.Series(nm.array(weekdays))
     joined['je_praznik'] = pd.Series(nm.array(holidays))
-    joined2 = joined.loc[joined['PRPV_Povrsje']!='SU']
+    '''
+    #joined2 = joined.loc[joined['PRPV_Povrsje'] != 'SU']
+    '''
+    return joined[['PRPO_poskodbe', 'Datum_Nesrece', 'Cas_Nesrece', 'Naselje_ali_ne', 'PRVZ_vzrok',
+                    'PRPV_Tip_Nesrece', 'PRVR_Vreme', 'PRSP_Promet', 'PRSV_vozisce', 'PRPV_Povrsje',
+                    'LOVC_vrsta_ceste', 'ime_ceste', 'ulica_odseka', 'stevilka_odseka', 'LVZN_vrsta_avta', 'kraj',
+                    'LVVN_vrsta_vozila', 'regija', 'mesec', 'dan', 'dan_v_tednu']]
+    '''
+    return joined
 
-    return joined2[['PRPO_poskodbe', 'Datum_Nesrece', 'Cas_Nesrece', 'Naselje_ali_ne', 'PRVZ_vzrok',
-                   'PRPV_Tip_Nesrece', 'PRVR_Vreme', 'PRSP_Promet', 'PRSV_vozisce', 'PRPV_Povrsje',
-                   'LOVC_vrsta_ceste', 'ime_ceste', 'ulica_odseka', 'stevilka_odseka', 'LVZN_vrsta_avta', 'kraj',
-                   'LVVN_vrsta_vozila', 'regija', 'mesec', 'dan', 'dan_v_tednu']]
+def refresh_data_to_send():
+    path ='../../podatki/vsi_podatki.csv'
+    data = pd.read_csv(path)
+    #itemsets, rules = apriori(dp.data_generator(path), min_support=0.2, min_confidence=0.5)
+    itemsets,rules = create_rules(data,path)
+    list_of_keys = list(itemsets[1].keys())
+    save_output(data, itemsets)
 
 
-test(read_join_data())
-itemsets, rules = create_rules()
-print(rules)
+def main():
+    #dp.strip_white_spaces('clean_data.csv')
+    #refresh_data_to_send()
+    '''
+    data = read_join_data('MPOSEBE20')
+    data.to_csv("temp.csv", encoding='utf-8', index=False)
+    dp.strip_white_spaces('temp.csv')
+    '''
+    data  = pd.read_csv('temp.csv')
+    data.to_csv('../../podatki/osebe_csv.csv',encoding='utf-8', index=False)
+
+
+
+if __name__ == "__main__":
+    main()
--- machine-learning/client/data_processer.py
+++ machine-learning/client/data_processer.py
@@ -1,4 +1,4 @@
-import codecs, json, requests, pandas as pd, numpy as nm, holidays as h,datetime
+import codecs, json, requests, pandas as pd, numpy as nm, holidays as h,datetime,csv
 from pandas.io.json import json_normalize
 
 # from url
@@ -60,4 +60,25 @@ def get_road_data(data):
     town = df['kraj'].value_counts()
     town_json = json.loads(town.to_json())
 
-    return sections_json, sections_number_json, town_json
+    region = df['regija'].value_counts()
+    region_json = json.loads(region.to_json())
+
+    return sections_json, sections_number_json, town_json,region_json
+
+def strip_white_spaces(path):
+    reader = csv.DictReader(
+        open(path),
+    )
+    first_row =next(reader)
+    reader = (
+        dict((k, v.strip()) for k, v in row.items() if v) for row in reader)
+
+    output_rows = []
+    for row in reader:
+        output_rows.append(row)
+    reader.close()
+    with open(path, 'w') as f:  # Just use 'w' mode in 3.x
+        w = csv.writer(f)
+        w.writerow(first_row)
+        for line in output_rows:
+            w.writerow(line.values())
