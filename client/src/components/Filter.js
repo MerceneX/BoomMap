14a44b5828e1abb3472eb81218ac2e7e2babf9b3
marusakonecnik <konecnik.10@gmail.com>
2019-06-09 12:01:20 +0200

    kriticni odseki na splosno prikazani

---
+++ client/src/components/Filter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Filter extends React.Component {
+    render()
+    {
+        return (
+            <option>
+                <select>Trenutno stanje</select>
+                <select>Vsi odseki</select>
+            </option>
+
+    );
+    }
+}
+
+export default Filter
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import 'leaflet/dist/leaflet.css'
+import Select from 'react-select'
 import Map from '../components/Map'
 
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const options = [
+    { value: 'currentState', label: 'Trenutno stanje' },
+    { value: 'critical_sections', label: 'Kritični odseki' },
+];
 
 class SimpleMap extends Component {
-    static defaultProps = {
-        center: {
-            lat: 45.91,
-            lng: 14.91,
-        },
-        zoom: 8.5
+    state = {
+        selectedOption: null,
+    };
+    handleChange = selectedOption => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
     };
 
     render() {
+        const { selectedOption } = this.state;
         return (
             // Important! Always set the container height explicitly
+
             <div align={"center"}>
+                <div align={"left"}>
+                    <Select
+                        value={selectedOption}
+                        onChange={this.handleChange}
+                        options={options}
+                    />
+                </div>
+                <br/>
                 <Map />
             </div>
         );
