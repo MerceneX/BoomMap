199c63f8b514f7d0cb8a1eb23e61a0dd1c61d523
Marko <sef.gluhak@gmail.com>
2019-06-12 10:39:47 +0100

    Dodani grafi 36 in 37, z realizacijo. Realiziran graf 35

--- podatki/unique_values.py
+++ podatki/unique_values.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 data = pd.read_csv("./podatki/vsi_podatki.csv", delimiter=",")
-unique_values = data['LVVN_vrsta_vozila'].unique()
+unique_values = data['PRPO_poskodbe'].unique()
 
 #data = pd.read_csv("podatki/osebe_csv.csv", delimiter=",")
 #unique_values = data['spol'].unique()
--- routes/graphRoutes.js
+++ routes/graphRoutes.js
@@ -77,35 +77,115 @@ router.get("/34", async (req, res) => {
 
 router.get("/35", async (req, res) => {
     //N/A
-    res.json({
-        "naslov": "Intenyivnosti po[kodb v nedeljo",
+    let result;
+    if(result)
+        res.json(result);
+    else {
+        result = {
+        "naslov": "Intenzivnosti poškodb v nedeljo",
         "podatki":
             [{
                 "intenzivnost": "BREZ POŠKODBE",
-                "nesrece": 3012
+                "nesrece": await QueryMan.customQuery("nesrece", ["dan_v_tednu","PRPO_poskodbe"],["6w","B"])+ await QueryMan.customQuery("nesrece", ["dan_v_tednu","PRPO_poskodbe"],["6w","U"])
             },
             {
                 "intenzivnost": "HUDA TELESNA POŠKODBA",
-                "nesrece": 3970
+                "nesrece": await QueryMan.customQuery("nesrece", ["dan_v_tednu","PRPO_poskodbe"],["6w","H"])
             },
             {
                 "intenzivnost": "LAŽJA TELESNA POŠKODBA",
-                "nesrece": 7051
+                "nesrece": await QueryMan.customQuery("nesrece", ["dan_v_tednu","PRPO_poskodbe"],["6w","L"])
             },
             {
                 "intenzivnost": "SMRT",
-                "nesrece": 81
+                "nesrece": await QueryMan.customQuery("nesrece", ["dan_v_tednu","PRPO_poskodbe"],["6w","S"])
             }]
-    });
+        };
+        res.json(result);
+    }
 });
 
 router.get("/36", async (req, res) => {
     //N/A
-    res.json({status: "to-be-done"});
+    let result;
+    if(result)
+        res.json(result);
+    else {
+        result = {
+        "naslov": "Intenzivnosti poškodb med prazniki",
+        "podatki":
+            [{
+                "intenzivnost": "BREZ POŠKODBE",
+                "nesrece": await QueryMan.customQuery("nesrece", ["je_praznik","PRPO_poskodbe"],[true,"B"])+ await QueryMan.customQuery("nesrece", ["je_praznik","PRPO_poskodbe"],[true,"U"])
+            },
+                {
+                    "intenzivnost": "HUDA TELESNA POŠKODBA",
+                    "nesrece": await QueryMan.customQuery("nesrece", ["je_praznik","PRPO_poskodbe"],[true,"H"])
+                },
+                {
+                    "intenzivnost": "LAŽJA TELESNA POŠKODBA",
+                    "nesrece": await QueryMan.customQuery("nesrece", ["je_praznik","PRPO_poskodbe"],[true,"L"])
+                },
+                {
+                    "intenzivnost": "SMRT",
+                    "nesrece": await QueryMan.customQuery("nesrece", ["je_praznik","PRPO_poskodbe"],[true,"S"])
+                }]
+        };
+        res.json(result);
+    }
 });
 
 router.get("/37", async (req, res) => {
     //N/A
-    res.json({status: "to-be-done"});
+    let result;
+    if(result)
+        res.json(result);
+    else {
+        result = {
+            "naslov": "Najbolj smrtonosni tip trčenja",
+            "podatki":
+                    [{
+                        "intenzivnost": "BOČNO TRČENJE",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["BT", "S"])
+                    },
+                    {
+                        "intenzivnost": "ČELNO TRČENJE",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["�T", "S"])
+                    },
+                    {
+                        "intenzivnost": "NALETNO TRČENJE",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["NT", "S"])
+                    },
+                    {
+                        "intenzivnost": "OPLAŽENJE",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["OP", "S"])
+                    },
+                    {
+                        "intenzivnost": "OSTALO",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["OS", "S"])
+                    },
+                    {
+                        "intenzivnost": "POVOŽENJE PEŠCA",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["PP", "S"])
+                    },
+                    {
+                        "intenzivnost": "PREVRNITEV VOZILA",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["PR", "S"])
+                    },
+                    {
+                        "intenzivnost": "POVOŽENJE ŽIVALI",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["PZ", "S"])
+                    },
+                    {
+                        "intenzivnost": "TRČENJE V OBJEKT",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["TO", "S"])
+                    },
+                    {
+                        "intenzivnost": "TRČENJE V STOJEČE / PARKIRANO VOZILO",
+                        "nesrece": await QueryMan.customQuery("nesrece", ["PRPV_Tip_Nesrece", "PRPO_poskodbe"], ["TV", "S"])
+                    }]
+        };
+        res.json(result);
+    }
 });
 module.exports = router;
