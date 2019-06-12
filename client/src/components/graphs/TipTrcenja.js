c1f3efc28387a2017ea3d871e6b8ca7eb16a716c
Natasa <natasa.feher@student.um.si>
2019-06-12 12:45:40 +0200

    Nov graf

---
+++ client/src/components/graphs/TipTrcenja.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import {
+    AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend
+} from 'recharts';
+import MyCard2 from '../MyCard2';
+
+import axios from 'axios';
+
+
+var data = [
+];
+
+export default class TipTrcenja extends PureComponent {
+
+    state = {
+        dataLeto: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/graph/37').then(res => {
+            this.setState({dataLeto: res.data});
+            console.log(res.data);
+            for(var key in this.state) {
+                data.push(this.state[key]);
+            }
+        });
+    }
+
+    render() {
+        return (
+            <AreaChart
+                width={1200}
+                height={300}
+                data={this.state.dataLeto.podatki}
+                margin={{
+                    top: 10, right: 30, left: 0, bottom: 0,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="intenzivnost" />
+                <YAxis />
+                <Tooltip />
+                <Legend/>
+                <Area type="monotone" dataKey="nesrece" stroke="#66b2b2" fill="#66b2b2" />
+            </AreaChart>
+        );
+    }
+}
