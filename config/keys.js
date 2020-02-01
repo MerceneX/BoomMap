80da74f6c2d5b77f8a9a2120191a541d4638dd95
MarkoTheStudent <marko.gluhak@student.um.si>
2020-02-01 15:46:25 +0100

    Added global dockerized database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/",
-    dbName: "BoomMap"
-}
+	mongoURI: "mongodb://mondmin:2fX1mk8K7gZBGZYq@164.8.221.27:27017/",
+	dbName: "BoomMap"
+};
