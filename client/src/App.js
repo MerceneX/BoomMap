14f04e020f9243ee73be34fac0c65bd3998ef660
Natasa <natasa.feher@student.um.si>
2020-03-21 19:11:06 +0100

    Spremembe front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,31 @@
 import React from 'react';
 import{BrowserRouter as Router, Route, withRouter} from 'react-router-dom'
 import './components/design/App.css';
-import Container from "reactstrap/es/Container";
-import Navigation from "./components/layout/Navigation";
-import Statistics from "./pages/Statistics";
-import About from "./pages/About";
 import Footer from "./components/layout/Footer";
+import Navigation from "./components/layout/Navigation";
 import Header from "./components/layout/Header";
-import Home from "./pages/Home";
-import 'leaflet/dist/leaflet.css'
+import FrontPage from "./pages/FrontPage";
+import 'leaflet/dist/leaflet.css';
+import Container from "reactstrap/es/Container";
 
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <Header/>
-                <Navigation/>
+        <div className="App">
 
+        <Header/>
+            <Navigation/>
 
-            <Route exact path="/" component={Home}/>
-            <Route path="/statistika" component={Statistics}/>
-
-                <Container>
-
-                </Container>
-
-                <Route path="/about" component={About}/>
-            </div>
-            <Footer/>
-        </Router>
+            <Route exact path="/" component={FrontPage}/>
+        <Container>
+            
+        </Container>
+           
+         </div>
+         <Footer/>
+    </Router>
+       
     );
 }
 export default App;
