091205eda777b8fd4291ba5b0dbf5078a2440d5e
Pina Gornik <pinagornik@Pinas-MacBook-Pro.local>
2020-03-23 13:25:36 +0100

    delo na dogodkih

--- client/src/pages/StanjeNaCestah.js
+++ client/src/pages/StanjeNaCestah.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from "react";
 import axios from "axios";
 
+import axios from "axios";
 const serverLocation = require("../config/keys.js").server;
 
 var data = [];
@@ -41,65 +42,3 @@ export default class StanjeNaCestah extends React.Component {
 		);
 	}
 }
-/*
-import React, { PureComponent } from "react";
-import axios from "axios";
-
-const serverLocation = require("../config/keys.js").server;
-
-var data = [];
-
-export default class StanjeNaCestah extends React.Component {
-	state = {
-		datag: []
-	};
-
-	componentDidMount() {
-		axios.get(`${serverLocation}/api/content/road-conditions`).then(res => {
-			this.setState(
-				{
-					datag: res.data
-				},
-				function() {
-					console.log("Datag updated");
-					console.log(this.state);
-				}
-			);
-			for (var key in this.state) {
-				data.push(this.state[key]);
-			}
-		});
-	}
-
-	componentWillUpdate() {
-		console.log("WillUpdating");
-		console.log(this.state);
-	}
-
-	render() {
-		console.log("Render called");
-		console.log(this.state.datag.items);
-		//const itemz = this.state.datag.items.map((item, key) => <h3>{item}</h3>);
-		let numbers;
-		//numbers = this.state.datag.items;
-		if (this.state.datag.items) {
-			numbers = this.state.datag.items.map(item => {
-				console.log("Iterating through for item " + item.title);
-				return <div>{item.title}</div>;
-			});
-		}
-		console.log("Logging numbers");
-		console.log(numbers);
-		return <ul>{numbers}</ul>;
-		/*return (
-			<div className="container">
-				<div className="col-xs-8">
-					<h1>{this.state.datag.title}</h1>
-					{itemz}
-				</div>
-			</div>
-		);
-	}
-}
-
-*/
