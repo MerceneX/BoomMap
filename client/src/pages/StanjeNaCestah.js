0023abc39b3133034b162cfefa725a1a29692dea
Pina Gornik <pinagornik@Pinas-MacBook-Pro.local>
2020-03-23 13:25:36 +0100

    delo na dogodkih

--- client/src/pages/StanjeNaCestah.js
+++ client/src/pages/StanjeNaCestah.js
@@ -1,47 +1,44 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
+
 import axios from 'axios';
-
 const serverLocation = require("../config/keys.js").server;
-
-
 var data = [
 ];
 
 export default class StanjeNaCestah extends React.Component {
+  state = {
+    persons: []
+  }
 
-    state = {
-        datag: [],
-    }
-
-
-   componentDidMount() {
-        axios.get(`${serverLocation}/api/content/borders`).then(res => {
-            this.setState({datag: res.data});
+  componentDidMount() {
+ //   axios.get(`https://jsonplaceholder.typicode.com/users`)
+     axios.get(`${serverLocation}/api/content/road-conditions`).then(res => {
+       //axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+            this.setState({persons: res.data});
            for(var key in this.state) {
                 data.push(this.state[key]);
 
             }
 
-        //    console.log(this.state.datag.title);
-            console.log(this.state.datag.items);
-        //    console.log(this.state[key]);
+//console.log(this.state.persons.items[0].title);
+//console.log(this.state.persons);
+
+
         });
-    }
+  }
 
+  render() {
+  console.log(this.state.persons.items);
+  const test = this.state.persons.items;
+  //console.log(test);
+test.forEach(item=>{
+     console.log(item);
+ });
+ //console.log(this.state.persons.title[0])
 
+    return (
+<p>hej</p>
 
-    render() {
-        return (
-
-
-       <div className="container">
-        <div className="col-xs-8">
-        <h1>{this.state.datag.title}</h1>
-
-        </div>
-       </div>
-
-
-        );
+        )
     }
 }
