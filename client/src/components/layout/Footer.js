3a0cea030bd1261dab8d69c29c9a98352f7eed2b
Natasa <natasa.feher@student.um.si>
2020-03-23 21:12:46 +0100

    Design spremembe

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -12,7 +12,7 @@ function Footer(props){
                 Vir podatkov: <a href="https://www.avp-rs.si/">avp-rs.si</a>
             </Typography>
             <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-                Kontakt: marko.gluhak@student.um.si
+                Kontakt: varnodomov.info@gmail.com
             </Typography>
         </footer>
     )
