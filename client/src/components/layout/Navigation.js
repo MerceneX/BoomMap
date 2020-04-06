3a82411fe28d79b9d66c140dc43ff649f46a765d
MarkoTheStudent <marko.gluhak@student.um.si>
2020-04-06 20:59:44 +0200

    Fixed the god damn navbar

--- client/src/components/layout/Navigation.js
+++ client/src/components/layout/Navigation.js
@@ -1,18 +1,66 @@
-import React from 'react';
-import '../design/App.css';
-import {Navbar, NavLink} from 'reactstrap';
+import React, { useState } from "react";
+import "../design/App.css";
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem,
+	NavbarText,
+} from "reactstrap";
 import { IoIosStats } from "react-icons/io";
-import {IoIosHome} from "react-icons/io";
-import {MdPerson} from "react-icons/md";
-import {FaNewspaper} from "react-icons/fa";
-import {FaMapMarkerAlt} from "react-icons/fa";
-import {FaRoad} from "react-icons/fa";
-import {FaCar} from "react-icons/fa";
+import { IoIosHome } from "react-icons/io";
+import { MdPerson } from "react-icons/md";
+import { FaNewspaper } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaRoad } from "react-icons/fa";
+import { FaCar } from "react-icons/fa";
 
-class Navigation extends React.Component {
-    render() {
-        return (
-            <div className="nav-div">
+const Navigation = (props) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
+	return (
+		<Navbar color="dark" dark expand="md" className="nav-bar">
+			<NavbarToggler onClick={toggle} />
+			<Collapse isOpen={isOpen} navbar>
+				<Nav expand="sm" navbar>
+					<NavLink href="/">
+						<IoIosHome /> Domov
+					</NavLink>
+					<NavLink href="/zemljevid">
+						<FaMapMarkerAlt />
+						Zemljevid
+					</NavLink>
+					<NavLink href="/statistika">
+						<IoIosStats /> Statistika
+					</NavLink>
+					<NavLink href="/prometnenovice">
+						<FaNewspaper /> Novice
+					</NavLink>
+					<NavLink href="/stanjenacestah">
+						<FaRoad /> Stanje na cestah
+					</NavLink>
+					<NavLink href="/napovedi">
+						<FaCar /> Napovedi prometa
+					</NavLink>
+					<NavLink href="/onas">
+						<MdPerson /> O nas
+					</NavLink>
+				</Nav>
+			</Collapse>
+		</Navbar>
+	);
+};
+export default Navigation;
+/*
+<div className="nav-div">
                 <Navbar expand="sm" className="nav-bar">
                         <NavLink href="/"><IoIosHome/> Domov </NavLink>
                         <NavLink href="/zemljevid"><FaMapMarkerAlt/>Zemljevid </NavLink>
@@ -22,8 +70,4 @@ class Navigation extends React.Component {
                         <NavLink href="/napovedi"> <FaCar/> Napovedi prometa</NavLink>
                         <NavLink href="/onas"><MdPerson/> O nas</NavLink>
                 </Navbar>
-            </div>
-        )
-    }
-}
-export default Navigation;
+            </div>*/
