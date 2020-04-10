e4c15fc7c58594729a4589ed01d2dcf32239547d
MarkoTheStudent <marko.gluhak@student.um.si>
2020-04-10 10:48:16 +0200

    Added and styled rss content metadata

--- client/src/components/rss/RoadEvents.js
+++ client/src/components/rss/RoadEvents.js
@@ -1,48 +1,63 @@
-
-import React from 'react';
+import React from "react";
 import axios from "axios";
 
 const serverLocation = require("../../config/keys.js").server;
-
 
 var data = [];
 var sliced = [];
 
 class DogodkiNaCestah extends React.Component {
-    state = {
-        datag: [],
-        title: []
-    };
+	state = {
+		datag: [],
+		title: [],
+	};
 
-    componentDidMount() {
-        axios.get(`${serverLocation}/api/content/road-events`).then(res => {
+	componentDidMount() {
+		axios.get(`${serverLocation}/api/content/road-events`).then((res) => {
+			this.setState({ datag: res.data.items.slice(0, 10) }, () =>
+				console.log("Updated state")
+			);
+			this.setState({ title: res.data }, () => console.log("Updated state"));
+			console.log(this.state.datag[0]);
 
-            this.setState({ datag: res.data.items.slice(0,10) }, () => console.log("Updated state"));
-            this.setState({ title: res.data }, () => console.log("Updated state"));
-            console.log(this.state.datag[0]);
+			for (var key in this.state) {
+				data.push(this.state[key]);
+			}
+		});
+	}
 
-            for (var key in this.state) {
-                data.push(this.state[key]);
-            }
-        });
-    }
-
-    render() {
-        let numbers;
-        console.log(this.state.datag[0]);
-        if (this.state.datag[0]) {
-            numbers = this.state.datag[0].map(item => {
-                console.log("Iterating through for item " + item.title);
-                return <div className="ContentStyle"><p><h5><b>{item.title}</b><hr/></h5> {item.description}<br /></p></div>;
-            });
-        }
-        return (
-            <div className="containerEvents">
-                <div className="col-xs-8">
-                    <ul>{numbers}</ul>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		let numbers;
+		console.log(this.state.datag[0]);
+		if (this.state.datag[0]) {
+			numbers = this.state.datag[0].map((item) => {
+				console.log("Iterating through for item " + item.title);
+				return (
+					<div className="ContentStyle">
+						<div className="contentHeader">
+							<div className="contentTitle">
+								<h5>
+									<b>{item.title}</b>
+								</h5>
+								<div className="contentCategory">{item.category}</div>
+							</div>
+							<p className="date">
+								{new Date(item.datePublished).toDateString()}
+							</p>
+						</div>
+						<hr />
+						<p className="cardContent">{item.description}</p>
+					</div>
+				);
+			});
+		}
+		return (
+			<div className="containerEvents">
+				<div className="col-xs-8">
+					<ul>{numbers}</ul>
+				</div>
+			</div>
+		);
+	}
 }
 export default DogodkiNaCestah;
