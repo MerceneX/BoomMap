888276ac4a803816cc6e3311a0621183090043be
MerceneX <sef.gluhak@gmail.com>
2022-05-07 17:18:21 +0200

    feat: added tests for query options

--- test/content_query_oprtions_test.js
+++ test/content_query_oprtions_test.js
@@ -16,15 +16,22 @@ describe('ContentQueryOptions ', () => {
         limit: 10,
         skip: 0
       };
+      let term = '';
+      let dStart = '2022-05-07T14:15:22.062Z';
+      let dEnd = '2022-04-09T14:15:22.062Z';
+      let sort = 'datePublished';
+      let sortAscDesc = -1;
+      let limit = 10;
+      let skip = 0;
 
       const actualResult = new ContentQueryOptions(
-        (term = ''),
-        (dStart = '2022-05-07T14:15:22.062Z'),
-        (dEnd = '2022-04-09T14:15:22.062Z'),
-        (sort = 'datePublished'),
-        (sortAscDesc = -1),
-        (limit = 10),
-        (skip = 0)
+        term,
+        dStart,
+        dEnd,
+        sort,
+        sortAscDesc,
+        limit,
+        skip
       );
       console.log(actualResult);
 
@@ -46,10 +53,7 @@ describe('ContentQueryOptions ', () => {
         (term = ''),
         (dStart = '2022-05-07T14:15:22.062Z'),
         (dEnd = '2022-04-09T14:15:22.062Z'),
-        (sort = ''),
-        sortAscDesc,
-        limit,
-        skip
+        (sort = '')
       );
       console.log(actualResult);
 
